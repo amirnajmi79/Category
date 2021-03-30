317a162d0693eb83d6c51c2f7bd3846511d9919f
amirnajmi79 <78718951+amirnajmi79@users.noreply.github.com>
2021-03-30 10:28:13 +0430

    Update 2021_03_26_151743_create_categories_table.php

--- src/migrations/2021_03_26_151743_create_categories_table.php
+++ src/migrations/2021_03_26_151743_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('type');
+            $table->softDeletes('deleted_at');
             $table->timestamps();
         });
     }
